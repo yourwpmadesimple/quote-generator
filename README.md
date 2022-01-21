d5be4ac70121f32ea8a43b1904c86b5b32d5a68e
Wayne Hatter <wayne@yourwpmadesimple.com>
2022-01-21 16:07:34 -0700

    send random text to the DOM

--- README.md
+++ README.md
@@ -42,3 +42,33 @@ function myLocalQuotes() {
 }
 myLocalQuotes();
 ```
+### Get elements
+```javascript
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+```
+
+### Set random Quote Text and Author to the DOM
+```javascript
+// Show New Quote
+function newQuote() {
+  // Pick a random quote form the apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  // Check if Author field is blank and replace it with 'Unknown'
+  if (quote.author === null) {
+    authorText.textContent = `~ Uknown`;
+  } else {
+    authorText.textContent = `~ ${quote.author}`; // Insert author text into DOM id=author
+  }
+  // Check if Quote text is more than 50 characters, then apply appropriate styling
+  if (quote.text.length > 50) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.textContent = quote.text; // Insert quote text into DOM id=quote
+}
+```
--- script.js
+++ script.js
@@ -1,20 +1,41 @@
 // Get Quotss From API
 // Asynchronous fetch request in try catch
 
-//let apiQuotes = [];
-
-// Show New Quote
-function newQuote() {
-  // Pick a random quote form the apiQuotes array
-  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  console.log(quote);
-}
-
+/*******
+ * Example of pulling data from local file
 function myLocalQuotes() {
   const quote = loadQuotes[Math.floor(Math.random() * loadQuotes.length)];
   console.log(quote);
 }
 myLocalQuotes();
+********** */
+
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
+let apiQuotes = [];
+
+// Show New Quote
+function newQuote() {
+  // Pick a random quote form the apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  // Check if Author field is blank and replace it with 'Unknown'
+  if (quote.author === null) {
+    authorText.textContent = `~ Uknown`;
+  } else {
+    authorText.textContent = `~ ${quote.author}`; // Insert author text into DOM id=author
+  }
+  // Check if Quote text is more than 50 characters, then apply appropriate styling
+  if (quote.text.length > 50) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.textContent = quote.text; // Insert quote text into DOM id=quote
+}
 
 async function getQuotes() {
   const apiUrl = "https://type.fit/api/quotes";
@@ -29,4 +50,4 @@ async function getQuotes() {
   }
 }
 // On Page Load
-//getQuotes();
+getQuotes();
