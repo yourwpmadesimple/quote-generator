b334b41a4b46f757e8a33cc78afff7e32c065e78
Wayne Hatter <wayne@yourwpmadesimple.com>
2022-01-21 15:19:18 -0700

    fetch with external file

--- README.md
+++ README.md
@@ -7,3 +7,13 @@ https://quotes-react.netlify.app/
 
 https://type.fit/api/quotes -> Actual API link for ^ service.
 ```
+### Using Math.random() and Math.floor() To Get Random Quotes From API
+[Math.random](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random)
+[Math.floor](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/floor)
+```
+function newQuote() {
+    // Pick a random quote form the apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote)
+}
+```
---
+++ script.js
@@ -0,0 +1,26 @@
+// Get Quotss From API
+// Asynchronous fetch request in try catch
+
+let apiQuotes = [];
+
+// Show New Quote
+function newQuote() {
+  // Pick a random quote form the apiQuotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(quote);
+}
+
+async function getQuotes() {
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiUrl); // Get json from API
+    // Global Variable 'apiQuotes'
+    apiQuotes = await response.json(); // Turn response into a json object
+    //console.log(apiQuotes[12]);
+    newQuote();
+  } catch (error) {
+    // Catch Error Here
+  }
+}
+// On Page Load
+getQuotes();
